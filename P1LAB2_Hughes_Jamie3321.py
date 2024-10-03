c090c637962dcc07b1412ae0f1c15b72de9c6381
Hughesj3321 <hughesj3321@student.faytechcc.edu>
2024-10-03 17:53:01 +0000

    p1lab2 abbed emojis

--- P1LAB2_Hughes_Jamie3321.py
+++ P1LAB2_Hughes_Jamie3321.py
@@ -31,7 +31,7 @@ meal_total = burger_total + fry_total
 # emoipedia has emojis to copy/paste
 # format() let us only show 2 decimal places
 print("-" * 20)
-print(num_burgers, "burger\t$", format(burger_total, ".2f"))
-print(num_fries,"fry\t\t$", format(fry_total, ".2f))
+print(num_burgers, "🍔burger\t$", format(burger_total, ".2f"))
+print(num_fries,"🍟fry\t\t$", format(fry_total, ".2f"))
 print("-" * 20)
 print("Total\t\t$", format(meal_total, ".2f"))
