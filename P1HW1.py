65181c313b61bc37af147f954f540ed66a24bccb
Hughesj3321 <hughesj3321@student.faytechcc.edu>
2024-10-03 18:56:24 +0000

    P1HW1 homework

---
+++ P1HW1.py
@@ -0,0 +1,12 @@
+# Hughes Jamie
+# 10/03/2024
+# P1HW1
+# Intergers
+
+value=int(input('enter value:'))
+exponent=int(input('enter exponent'))
+print('Answer is', value**exponent)
+start=int(input('enter value:'))
+add=int(input('enter value:'))
+subtract=int(input('enter value:'))
+print('Answer is', start+add-subtract)
