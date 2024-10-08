443a4bec84caf8d8db98dff23aad51f09afe2d88
Hughesj3321 <hughesj3321@student.faytechcc.edu>
2024-10-08 17:46:03 +0000

    Start to budget assignment

---
+++ P1HW2_Hughes.py
@@ -0,0 +1,22 @@
+# CTI 110
+# P1HW2
+# J Hughes
+# 10/08/24
+
+# This program is a travel budget
+"""
+3. Ask user to enter their budget
+4. Ask user to enter travel destination
+5. Ask user for amount they will spend on gas
+6. Ask user for amount they will spend on accommodation
+7. Ask user for amount they will spend on food
+8. Add expenses
+9. Subtract expenses from budget
+10. Display results
+"""
+
+# Ask user to enter their budget
+print("✈️Trip calculator✈️")
+budget = float(input("What is your travel budget? $ "))
+# Where are they going?
+destination = input("Where are you going?")
