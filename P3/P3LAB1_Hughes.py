29a5a8c8679d48f759940f95d9ad8ba2bdc3cad9
Hughesj3321 <hughesj3321@student.faytechcc.edu>
2024-10-22 18:30:38 +0000

    P3LAB1 spooky story

--- P3/P3LAB1_Hughes.py
+++ P3/P3LAB1_Hughes.py
@@ -5,7 +5,27 @@
 
 def main():
     print("Choose your own adventure")
-    go_home()
+    print("Your friends want to explore a spooky house.")
+    print("1. Go with friends.")
+    print("2. Go home.")
+    choice = int(input())
+    if choice ==1:
+        go_with_friends()
+    elif choice ==2:
+        go_home()
+
+def go_with_friends():
+    print("You rock, paper, siccors who goes to the door.")
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Siccors")
+    choice = int(input())
+    if choice ==1:
+        print("John goes to the door...he dies.")
+    if choice ==2:
+        print("Chris goes to the door...he dies.")
+    if choice ==3:
+        print("You go to the door and you sence that someone is watching you.")
 
 def go_home():
     print(" You decide to go home.")
